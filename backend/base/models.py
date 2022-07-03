d75e79dfc7bbfdabe10f707499c3a6f4d46916e0
Sunkanmi <leosuky99@gmail.com>
2022-07-03 02:31:21 +0100

    updated backend with products

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,11 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    # FURNITURE TYPES
+    FURNITURE_TYPE = (
+        ('Wood', 'Wood'),
+        ('Iron', 'Iron'),
+        ('Ceramic', 'Ceramic'),
+        ('Material', 'Material'),
+        ('Glass', 'Glass'),
+    )
+    FURNITURE_CATEGORIES = (
+        ('Living Room', 'Living Room'),
+        ('Kitchen', 'Kitchen'),
+        ('Bathroom', 'Bathroom'),
+        ('Bedroom', 'Bedroom'),
+        ('Workspace', 'Workspace'),
+        ('Accessories', 'Accessories'),
+    )
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    type = models.CharField(max_length=200, null=True, blank=True)
-    category = models.CharField(max_length=200, null=True, blank=True)
+    type = models.CharField(max_length=200, choices=FURNITURE_TYPE, null=True, blank=True)
+    category = models.CharField(max_length=200, choices=FURNITURE_CATEGORIES, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True
